39f7f6f5e8ac42f4a418881a265170a2e9503820
christian birego <93344052+birego@users.noreply.github.com>
2024-05-07 13:57:09 +0200

    Ajout du corse()

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import route from './routes/twitter.routes.js';
+import corse from 'cors';
 dotenv.config();
 const {port} = process.env || 4400;
 const app = express();
+app.use(corse()) //middleware corse
 app.use(express.json())
 
 app.use("/",route)
